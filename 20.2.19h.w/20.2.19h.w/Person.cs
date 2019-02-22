25e1dff4da798a2959a0dc90fe0d0660a663387d
sagitush <44786027+sagitush@users.noreply.github.com>
2019-02-22 12:05:09 +0200

    Add files via upload

---
+++ 20.2.19h.w/20.2.19h.w/Person.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._2._19h.w
+{
+    class Person : IComparable<Person>
+    {
+        public int Id { get; private set; }
+        public int Age { get; private set; }
+        public float Height { get; private set; }
+        public string Name { get; private set; }
+
+     
+        public Person(int id, int age, float height, string name)
+        {
+            Id = id;
+            Age = age;
+            Height = height;
+            Name = name;
+        }
+        private static readonly IComparer<Person> idComparer;
+        private static readonly IComparer<Person> ageComparer;
+        private static readonly IComparer<Person> heightComparer;
+        private static readonly IComparer<Person> nameComparer;
+
+        public IComparer<Person> IDComparer
+        {
+            get
+            {
+                return Person.idComparer;
+            }
+        }
+        public IComparer<Person> AgeComparer
+        {
+            get
+            {
+                return Person.ageComparer;
+            }
+        }
+        public IComparer<Person> HeightComparer
+        {
+            get
+            {
+                return Person.heightComparer;
+            }
+        }
+        public IComparer<Person> NameComparer
+        {
+            get
+            {
+                return Person.nameComparer;
+            }
+        }
+
+        static Person()
+        {
+            
+
+
+        }
+
+        public static void ModifyDefaultComparer(string b)
+        {
+
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"Person name:{Name}, ID:{Id}, Age:{Age}, Height:{Height}";
+        }
+
+
+        public int CompareTo(Person other)
+        {
+            return this.Id - other.Id;
+        }
+    }
+    class PersonCompareById : IComparer<Person>
+    {
+
+        public int Compare(Person x, Person y)
+        {
+            return x.Id - y.Id;
+        }
+
+
+    }
+    class PersonCompareByAge : IComparer<Person>
+    {
+
+        public int Compare(Person x, Person y)
+        {
+            return x.Age - y.Age;
+        }
+  
+
+    }
+    class PersonCompareByHeight : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            if (x.Height-y.Height==0)
+            {
+                return 0;
+            }
+            else if(x.Height-y.Height>0)
+            {
+                return 1;
+            }
+            else 
+            {
+                return -1;
+            }
+          
+        }
+    }
+    class PersonCompareByName : IComparer<Person>
+    {
+
+        public int Compare(Person x, Person y)
+        {
+            return x.Name.CompareTo(y.Name);
+        }
+
+
+    }
+
+}
---
+++ 20.2.19h.w/20.2.19h.w/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._2._19h.w
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person itamar = new Person(335, 34, 65.2f, "Itamar");
+            Person noam = new Person(555, 9, 20.2f, "Noam");
+            Person shir = new Person(777, 6, 17.2f, "Shir");
+            Person alma = new Person(222, 2, 10.2f, "Alma");
+            Person sagit = new Person(666, 34, 50.2f, "Sagit");
+            Person[] persons = { itamar, noam, shir, alma, sagit };
+            Array.Sort(persons, new PersonCompareByName());
+            Console.WriteLine("name:");
+            PrintPersonArray(persons);
+            Array.Sort(persons, new PersonCompareById());
+            Console.WriteLine("ID:");
+            PrintPersonArray(persons);
+            Array.Sort(persons, new PersonCompareByHeight());
+            Console.WriteLine("Height:");
+            PrintPersonArray(persons);
+            Array.Sort(persons, new PersonCompareByAge());
+            Console.WriteLine("Age");
+            PrintPersonArray(persons);
+
+        }
+        static void PrintPersonArray(Person[]people)
+        {
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person);
+            }
+            
+        }
+    }
+}
